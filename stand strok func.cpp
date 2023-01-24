88db57e67b33e1a198b0fe7ed23ac0bdc510858c
ybeshkov <91119020+ybeshkov@users.noreply.github.com>
2023-01-24 20:36:55 +0300

    Add files via upload

---
+++ stand strok func.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string S, S0;
+    cout << "введите строку S:\n";
+    getline(cin, S);
+    cout << "введите подстроку S0:\n";
+    getline(cin, S0);
+    if (S.find(S0) != string::npos) cout << S.erase(S.find(S0), S0.size()) << endl;
+    else cout << S << endl;
+    system("pause");
+    return 0;
+}
